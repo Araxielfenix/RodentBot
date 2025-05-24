07f76b1f0cac81511691f2332f128515cab56a8d
Araxielfenix <araxielfenix@outlook.com>
2025-05-23 21:33:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ client.on("messageCreate", async (message) => {
 
     await message.channel.sendTyping();
 
-    if(message.content.toLowerCase().startsWith(kw.toLowerCase()).includes("!imagine")){
+    if(message.content.toLowerCase().startsWith("!imagine"){
       message.react("🎨");
     }
 
